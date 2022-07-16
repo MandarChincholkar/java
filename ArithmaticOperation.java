114f5342f4cd159f6810e48ed12651f98f33dc37
Mandar Chincholkar <59415195+MandarChincholkar@users.noreply.github.com>
2022-07-16 18:17:26 +0530

    changes for jenkins  purpose

--- ArithmaticOperation.java
+++ ArithmaticOperation.java
@@ -5,10 +5,10 @@ public class ArithmaticOperation{
 	public static void main(String args[]){
 	
 	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter 2 positive numbers :");
-
-	int a = scan.nextInt();
-	int b = scan.nextInt();
+	//System.out.println("Enter 2 positive numbers :");
+	int a =10, b = 2;
+	//int a = scan.nextInt();
+	//int b = scan.nextInt();
 	System.out.println(a + "+" + b + " = " + (a+b));
 	System.out.println(a + "-" + b + " = " + (a-b));
 	System.out.println(a + "*" + b + " = " + (a*b));
